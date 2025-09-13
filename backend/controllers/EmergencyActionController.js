68377d9100be6794b7ee52594554453c0b7d056c
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2025-09-13 16:16:44 +0800

    feat: add profile_uuid & profile_label

--- backend/controllers/EmergencyActionController.js
+++ backend/controllers/EmergencyActionController.js
@@ -1,26 +1,41 @@
 import UserBroswer from "../models/UserBroswer.js";
 
 export const checkEmergencyAction = async (req, res) => {
-    const broswerId = req.headers['x-broswer-id'];
-    console.log("Received broswerId:", broswerId);
-    if (!broswerId) {
+    const browserId = req.headers['x-broswer-id'];
+    const profileUuid = req.headers['x-profile-uuid'];
+    
+    console.log("Received browserId:", browserId);
+    console.log("Received profileUuid:", profileUuid);
+    
+    if (!browserId) {
         return res.json({
-            message: "Something wrong! Broswer Id not exit! Contact to developer.",
+            message: "Something wrong! Browser Id not exit! Contact to developer.",
             emergency_action: false
         });
     }
-    const broswerName = req.headers['user-agent'] || "Unknown";
-    console.log("Authenticated call to emergency endpoint, broswerName:", broswerName);
+    
+    if (!profileUuid) {
+        return res.json({
+            message: "Something wrong! Profile UUID not exit! Contact to developer.",
+            emergency_action: false
+        });
+    }
+    const browserName = req.headers['user-agent'] || "Unknown";
+    console.log("Authenticated call to emergency endpoint, browserName:", browserName);
 
     try {
         const record = await UserBroswer.findOne({
-            broswer_id: broswerId,
+            broswer_id: browserId,
+            profile_uuid: profileUuid,
             user_id: req.user._id
-        }).select("emergency_action");
-        console.log("record: ",JSON.stringify(record));
+        }).select("emergency_action profile_uuid profile_label");
+        console.log("record: ", JSON.stringify(record));
+        
         return res.status(200).json({
             message: record?.emergency_action ? "Clear all data!" : "No emergency action required.",
-            emergency_action: record?.emergency_action
+            emergency_action: record?.emergency_action || false,
+            profile_uuid: record?.profileUuid,
+            profile_label: record?.profile_label
         });
     } catch (err) {
         if (err) return res.status(403).json({ error: "Fail to call Emergency Action!" });
--- backend/models/UserBroswer.js
+++ backend/models/UserBroswer.js
@@ -14,6 +14,14 @@ const UserBroswerSchema = new mongoose.Schema({
       type: Boolean,
       default: false,
   },
+  profile_uuid: {
+      type: String,
+      required: true,
+  },
+  profile_label: {
+      type: String,
+      default: null,
+  }
 });
 
 const UserBroswer = mongoose.model('UserBroswer', UserBroswerSchema);
